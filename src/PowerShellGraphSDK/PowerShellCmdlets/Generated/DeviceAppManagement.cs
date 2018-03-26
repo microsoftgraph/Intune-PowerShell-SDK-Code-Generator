49fb69786879dc5213cbebe1b5effec8009cb674
Rohit Ramu <roramu@microsoft.com>
2018-03-26 16:19:33 -0700

    Allow hand-written cmdlets to exist in the PowerShellGraphSDK project

---
+++ src/PowerShellGraphSDK/PowerShellCmdlets/Generated/DeviceAppManagement.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+
+namespace PowerShellGraphSDK.PowerShellCmdlets
+{
+    using System.Management.Automation;
+
+    [Cmdlet(
+        CmdletVerb, CmdletNoun,
+        ConfirmImpact = ConfirmImpact.Low)]
+    public class GetDeviceAppManagement : ODataGetPowerShellSDKCmdlet
+    {
+        public const string CmdletVerb = VerbsCommon.Get;
+        public const string CmdletNoun = "IntuneDeviceAppManagement";
+
+        internal override string GetResourcePath()
+        {
+            return "deviceAppManagement";
+        }
+    }
+
+    // TODO: Implement PATCH on deviceAppManagement
+
+    //[Cmdlet(
+    //    VerbsData.Update, "GraphDeviceAppManagement",
+    //    ConfirmImpact = ConfirmImpact.Medium)]
+    //public class UpdateDeviceAppManagement : ODataPowerShellSDKCmdlet
+    //{
+    //    [Parameter(ValueFromPipeline = true, ValueFromPipelineByPropertyName = true)]
+    //    public DateTime microsoftStoreForBusinessLastSuccessfulSyncDateTime { get; set; }
+
+    //    [Parameter(ValueFromPipeline = true, ValueFromPipelineByPropertyName = true)]
+    //    public bool? isEnabledForMicrosoftStoreForBusiness { get; set; }
+
+    //    [Parameter(ValueFromPipeline = true, ValueFromPipelineByPropertyName = true)]
+    //    public string microsoftStoreForBusinessLanguage { get; set; }
+
+    //    [Parameter(ValueFromPipeline = true, ValueFromPipelineByPropertyName = true)]
+    //    public DateTime microsoftStoreForBusinessLastCompletedApplicationSyncTime { get; set; }
+
+    //    internal override HttpMethod GetHttpMethod()
+    //    {
+    //        return new HttpMethod("PATCH");
+    //    }
+
+    //    internal override string GetResourcePath()
+    //    {
+    //        return "deviceAppManagement";
+    //    }
+
+    //    internal override object GetContent()
+    //    {
+    //        // TODO: Convert parameters to a patch set
+    //        throw new NotImplementedException();
+    //    }
+    //}
+}
