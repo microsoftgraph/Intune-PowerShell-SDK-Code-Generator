90a1fd7bcd2f3547a506f8887c84e77b7d383574
Rohit Ramu <roramu@microsoft.com>
2018-09-07 11:24:14 -0700

    Fix serialization of nested arrays

---
+++ Tests/DeviceManagement/DeviceCompliancePolicies.ps1
@@ -0,0 +1,7 @@
+# Create a compliance policy
+Write-Host 'Creating a compliance policy'
+$compliancePolicy = New-DeviceManagement_DeviceCompliancePolicies -iosCompliancePolicy -displayName "Chicago" -scheduledActionsForRule (New-DeviceComplianceScheduledActionForRuleObject -ruleName test -scheduledActionConfigurations (New-DeviceComplianceActionItemObject -gracePeriodHours 0 -actionType block -notificationTemplateId ""))
+
+# Remove the compliance policy
+Write-Host 'Deleting compliance policy'
+$compliancePolicy | Remove-DeviceManagement_DeviceCompliancePolicies
