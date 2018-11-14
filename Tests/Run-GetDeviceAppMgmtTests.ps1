75d4c5743ad599f49342b4816ca2ee21a718ed83
NSoy-MSFT <nsoy@microsoft.com>
2018-11-13 17:33:58 -0800

    Fix Tests

--- Tests/Run-GetDeviceAppMgmtTests.ps1
+++ Tests/Run-GetDeviceAppMgmtTests.ps1
@@ -1,13 +1,13 @@
 #
 # Import the Intune PowerShell SDK Module if necessary
 #
-if ((Get-Module 'Microsoft.Graph.Intune') -eq $null)
+if ((Get-Module 'Microsoft.Graph.Intune') -eq $env:null)
 {
     #
     # BUGBUG: Pass this as parameter on cmdline
     #
-    $moduleInstallFolderPath="..\src\GraphODataPowerShellWriter\bin\Release\output\bin\Release\net471"
-    Import-Module $moduleInstallFolderPath\Microsoft.Graph.Intune.psd1
+    $env:moduleInstallFolderPath="..\src\GraphODataPowerShellWriter\bin\Release\output\bin\Release\net471"
+    Import-Module $env:moduleInstallFolderPath\Microsoft.Graph.Intune.psd1
 }
 
 #
@@ -35,19 +35,19 @@ $tests = @(
 #
 # ManagedEBooks
 #
-    {$managedEBook = (Get-ManagedEBooks| Get-MSGraphAllPages)[0]}
-    {$managedEBook | Get-ManagedEBookAssignments}
-    {$managedEBook | Get-ManagedEBooksDeviceStates}
-    {$managedEBook | Get-ManagedEBooksInstallSummary}
-    {$managedEBooksUserState = (Get-ManagedEBooks| Get-MSGraphAllPages)[0] | Get-ManagedEBooksUserStateSummary}
-    {if ($managedEBooksUserState -ne $null) {(Get-ManagedEBooks| Get-MSGraphAllPages)[0] | Get-ManagedEBooksUserStateSummaryDeviceStates}}
+    {$env:managedEBook = (Get-ManagedEBooks| Get-MSGraphAllPages)[0]}
+    {(Get-ManagedEBooks| Get-MSGraphAllPages)[0] | Get-ManagedEBookAssignments}
+    {(Get-ManagedEBooks| Get-MSGraphAllPages)[0] | Get-ManagedEBooksDeviceStates}
+    {(Get-ManagedEBooks| Get-MSGraphAllPages)[0] | Get-ManagedEBooksInstallSummary}
+    {$env:managedEBooksUserState = (Get-ManagedEBooks| Get-MSGraphAllPages)[0] | Get-ManagedEBooksUserStateSummary}
+    {if ($env:managedEBooksUserState -ne $env:null) {(Get-ManagedEBooks| Get-MSGraphAllPages)[0] | Get-ManagedEBooksUserStateSummaryDeviceStates}}
 #
 # mdmWindowsInformationProtectionPolicy
 #
-    {$mdmWindowsInfoProtPolicy = (Get-MdmWindowsInformationProtectionPolicies | Get-MSGraphAllPages)[0]}
-    {$mdmWindowsInfoProtPolicy | Get-MdmWindowsInformationProtectionPoliciesAssignments}
-    {$mdmWindowsInfoProtPolicy | Get-MdmWindowsInformationProtectionPoliciesExemptAppLockerFiles}
-    {$mdmWindowsInfoProtPolicy | Get-MdmWindowsInformationProtectionPoliciesProtectedAppLockerFiles}
+    {$env:mdmWindowsInfoProtPolicy = (Get-MdmWindowsInformationProtectionPolicies | Get-MSGraphAllPages)[0]}
+    #BUGBUG: Missing Route {(Get-MdmWindowsInformationProtectionPolicies | Get-MSGraphAllPages)[0] | Get-MdmWindowsInformationProtectionPoliciesAssignments}
+    #BUGBUG: Missing Route {(Get-MdmWindowsInformationProtectionPolicies | Get-MSGraphAllPages)[0] | Get-MdmWindowsInformationProtectionPoliciesExemptAppLockerFiles}
+    #BUGBUG: Missing Route {(Get-MdmWindowsInformationProtectionPolicies | Get-MSGraphAllPages)[0] | Get-MdmWindowsInformationProtectionPoliciesProtectedAppLockerFiles}
 #
 # managedAppPolicies
 #
@@ -79,18 +79,18 @@ $tests = @(
 #
 # MobileAppConfigurations
 #    
-    {$mobileAppConfig = (Get-MobileAppConfigurations | Get-MSGraphAllPages)[0])}
-    {$mobileAppConfig | Get-MobileAppConfigurationAssignments}
-    {$mobileAppConfig | Get-MobileAppConfigurationDeviceStatuses}
-    {$mobileAppConfig | Get-MobileAppConfigurationDeviceStatusSummary}
-    {$mobileAppConfig | Get-MobileAppConfigurationsUserStatuses}
-    {$mobileAppConfig | Get-MobileAppConfigurationsUserStatusSummary}
+    {$env:mobileAppConfig = (Get-MobileAppConfigurations | Get-MSGraphAllPages)[0]}
+    {(Get-MobileAppConfigurations | Get-MSGraphAllPages)[0] | Get-MobileAppConfigurationAssignments}
+    {(Get-MobileAppConfigurations | Get-MSGraphAllPages)[0] | Get-MobileAppConfigurationDeviceStatuses}
+    {(Get-MobileAppConfigurations | Get-MSGraphAllPages)[0] | Get-MobileAppConfigurationDeviceStatusSummary}
+    {(Get-MobileAppConfigurations | Get-MSGraphAllPages)[0] | Get-MobileAppConfigurationsUserStatuses}
+    {(Get-MobileAppConfigurations | Get-MSGraphAllPages)[0] | Get-MobileAppConfigurationsUserStatusSummary}
 #
 # MobileApps
 #
-    {(Get-DeviceAppManagement_MobileApps | Get-MSGraphAllPages).Where({$_.displayName -Match 'Intune Managed Browser'}) | Get-DeviceAppManagement_MobileApps_Assignments}
-    {(Get-DeviceAppManagement_MobileApps | Get-MSGraphAllPages).Where({$_.displayName -Match 'Intune Managed Browser'}) | Get-DeviceAppManagement_MobileApps_Categories}        
-    #{(Get-DeviceAppManagement_MobileApps | Get-MSGraphAllPages).Where({$_.displayName -Match 'Intune Managed Browser'}) | Get-DeviceAppManagement_MobileApps_ContentVersionsFiles}
+    {(Get-DeviceAppManagement_MobileApps | Get-MSGraphAllPages).Where({$env:_.displayName -Match 'Intune Managed Browser'}) | Get-DeviceAppManagement_MobileApps_Assignments}
+    {(Get-DeviceAppManagement_MobileApps | Get-MSGraphAllPages).Where({$env:_.displayName -Match 'Intune Managed Browser'}) | Get-DeviceAppManagement_MobileApps_Categories}        
+    #{(Get-DeviceAppManagement_MobileApps | Get-MSGraphAllPages).Where({$env:_.displayName -Match 'Intune Managed Browser'}) | Get-DeviceAppManagement_MobileApps_ContentVersionsFiles}
 #
 # TargetedManagedAppConfigurations
 #
@@ -113,7 +113,7 @@ $tests = @(
 #
 try
 {
-    $msGraphMeta = Get-MSGraphMetadata
+    $env:msGraphMeta = Get-MSGraphMetadata
 }
 catch
 {    
@@ -130,7 +130,7 @@ foreach ($test in $tests)
 {
     try
     {        
-        $output = Invoke-Command -scriptblock $($test)
+        $output = Invoke-Command -scriptblock $test
         Write-Output "$test, $output"
     }
     catch
